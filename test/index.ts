5219670872e13b1ef1284dad1f91eea5b4f07c55
asimaranov <a.simaranov@gmail.com>
2022-04-12 15:14:12 +0300

    Made tests more clear

--- test/index.ts
+++ test/index.ts
@@ -17,12 +17,13 @@ describe("ItPubToken contract", function () {
   let ItPubTokenContract;
   let itPubTokenContract: Contract;
   let owner: SignerWithAddress, user1: SignerWithAddress, user2: SignerWithAddress, users: SignerWithAddress[];
+
   beforeEach(async () => {
     ItPubTokenContract = await ethers.getContractFactory('ItPubToken');
     [owner, user1, user2, ...users] = await ethers.getSigners();
     itPubTokenContract = await ItPubTokenContract.deploy();
-
   })
+  
   describe("Initial params of contract", async () => {
     it("Check total supply correctness", async () => {
       expect(await itPubTokenContract.totalSupply()).to.equal(TOTAL_SUPPLY);
@@ -65,50 +66,58 @@ describe("ItPubToken contract", function () {
       expect(itPubTokenContract.transfer(user1.address, TOTAL_SUPPLY + 1)).to.be.revertedWith('Not enough money');
       expect(await itPubTokenContract.balanceOf(user1.address)).to.equal(initialUserBalance);
     })
-    it("Check approving", async () => {
 
-      {
-        expect(await itPubTokenContract.allowance(owner.address, user1.address)).to.equal(0);
-        let approvingTransaction = await itPubTokenContract.approve(user1.address, TOKENS_TO_TRANSFER);
-        expect(await itPubTokenContract.allowance(owner.address, user1.address)).to.equal(TOKENS_TO_TRANSFER);
+    it("Check approving correctness", async () => {
+      expect(await itPubTokenContract.allowance(owner.address, user1.address)).to.equal(0);
+      let approvingTransaction = await itPubTokenContract.approve(user1.address, TOKENS_TO_TRANSFER);
+      expect(await itPubTokenContract.allowance(owner.address, user1.address)).to.equal(TOKENS_TO_TRANSFER);
 
-        let rcApproving = await approvingTransaction.wait();
-        const approvalEvent = rcApproving.events.find((event: { event: string; }) => event.event === 'Approval');
-        const [_owner, _spender, _value] = approvalEvent.args;
-        expect(_owner).to.equal(owner.address);
-        expect(_spender).to.equal(user1.address);
-        expect(_value).to.equal(TOKENS_TO_TRANSFER);
-      }
-
-      expect(itPubTokenContract.transferFrom(owner.address, user1.address, TOKENS_TO_TRANSFER + 1)).to.be.revertedWith("Not permitted");
-
-      {
-        const transferFromTransaction = await itPubTokenContract.transferFrom(owner.address, user1.address, TOKENS_TO_TRANSFER);
-
-        const rc = await transferFromTransaction.wait();
-
-        const transferEvent = rc.events.find((event: { event: string; }) => event.event === 'Transfer');
-        const [_from, _to, _value] = transferEvent.args;
-        expect(_from).to.equal(owner.address);
-        expect(_to).to.equal(user1.address);
-        expect(_value).to.equal(TOKENS_TO_TRANSFER);
-      }
-
-      {
-        await itPubTokenContract.approve(user1.address, TOTAL_SUPPLY + 1);
-        expect(itPubTokenContract.transferFrom(owner.address, user1.address, TOTAL_SUPPLY + 1)).to.be.revertedWith("Not enough money");
-
-      }
+      let rcApproving = await approvingTransaction.wait();
+      const approvalEvent = rcApproving.events.find((event: { event: string; }) => event.event === 'Approval');
+      const [_owner, _spender, _value] = approvalEvent.args;
+      expect(_owner).to.equal(owner.address);
+      expect(_spender).to.equal(user1.address);
+      expect(_value).to.equal(TOKENS_TO_TRANSFER);
     });
 
-    it("Check burn", async () => {
-      const initialUserBalance = await itPubTokenContract.balanceOf(owner.address);
 
+    it("Check that transferFrom fails if not allowed", async () => {
+      expect(itPubTokenContract.transferFrom(owner.address, user1.address, 1)).to.be.revertedWith("Not permitted");
+      await itPubTokenContract.approve(user1.address, TOKENS_TO_TRANSFER);
+      expect(itPubTokenContract.transferFrom(owner.address, user1.address, TOKENS_TO_TRANSFER + 1)).to.be.revertedWith("Not permitted");
+
+    })
+
+    it("Check transferFrom after approving", async () => {
+      await itPubTokenContract.approve(user1.address, TOKENS_TO_TRANSFER);
+
+      const transferFromTransaction = await itPubTokenContract.transferFrom(owner.address, user1.address, TOKENS_TO_TRANSFER);
+
+      const rc = await transferFromTransaction.wait();
+
+      const transferEvent = rc.events.find((event: { event: string; }) => event.event === 'Transfer');
+      const [_from, _to, _value] = transferEvent.args;
+      expect(_from).to.equal(owner.address);
+      expect(_to).to.equal(user1.address);
+      expect(_value).to.equal(TOKENS_TO_TRANSFER);
+
+    })
+
+    it("Check transferFrom with not enough money", async () => {
+      await itPubTokenContract.approve(user1.address, TOTAL_SUPPLY + 1);
+      expect(itPubTokenContract.transferFrom(owner.address, user1.address, TOTAL_SUPPLY + 1)).to.be.revertedWith("Not enough money");
+    })
+
+    it("Check burn correctness", async () => {
+      const initialUserBalance = await itPubTokenContract.balanceOf(owner.address);
       await itPubTokenContract.burn(TOKENS_TO_BURN);
       expect(await itPubTokenContract.totalSupply()).to.equal(TOTAL_SUPPLY - TOKENS_TO_BURN);
       expect(initialUserBalance - await itPubTokenContract.balanceOf(owner.address)).to.equal(TOKENS_TO_BURN);
+    })
 
-      expect(itPubTokenContract.burn(TOTAL_SUPPLY)).to.be.revertedWith('Not enough money')
+    it("Check that burn fails if not enough money", async () => {
+      await itPubTokenContract.balanceOf(owner.address);
+      expect(itPubTokenContract.burn(TOTAL_SUPPLY + 1)).to.be.revertedWith('Not enough money');
     })
 
   })
